9d0911f6746ef817da92e5182790710281e9f48d
kapsner <lorenz.kapsner@web.de>
2020-06-06 15:30:35 +0200

    updated again main.yml; commented out 'do_drat' from tic.R to check if pipeline is working now

--- tic.R
+++ tic.R
@@ -2,7 +2,7 @@
 if (!ci_has_env("PARAMTEST")) {
   do_package_checks()
 
-  do_drat("mlr3learners/mlr3learners.drat")
+  #do_drat("mlr3learners/mlr3learners.drat")
 } else {
   # PARAMTEST
   get_stage("install") %>%
