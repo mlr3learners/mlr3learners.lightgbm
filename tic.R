71197347d72fd78bda8e456e7cb7fcd20cdc47de
kapsner <lorenz.kapsner@web.de>
2020-06-07 14:18:22 +0200

    Update tic.R

--- tic.R
+++ tic.R
@@ -5,7 +5,7 @@ if (!ci_has_env("PARAMTEST")) {
   get_stage("install") %>%
     add_step(step_install_deps()) %>%
     add_code_step(git2r::clone("https://github.com/microsoft/LightGBM", "lightgbm")) %>%
-    add_code_step(system("Rscript -e 'print(paste0(getwd(), \"/lightgbm\")); setwd(paste0(getwd(), \"/lightgbm\")); system(\"Rscript build_r.R\")'"))
+    add_code_step(system("Rscript -e 'setwd(file.path(getwd(), \"lightgbm\")); source(\"build_r.R\")'"))
 
   #do_drat("mlr3learners/mlr3learners.drat")
 } else {
@@ -13,7 +13,7 @@ if (!ci_has_env("PARAMTEST")) {
   get_stage("install") %>%
     add_step(step_install_deps()) %>%
     add_code_step(git2r::clone("https://github.com/microsoft/LightGBM", "lightgbm")) %>%
-    add_code_step(system("Rscript -e 'setwd(paste0(getwd(), \"/lightgbm\")); system(\"Rscript build_r.R\")'"))
+    add_code_step(system("Rscript -e 'setwd(file.path(getwd(), \"lightgbm\")); source(\"build_r.R\")'"))
 
   get_stage("script") %>%
     add_code_step(testthat::test_dir(system.file("paramtest", package = "mlr3learners.lightgbm"),
